c368c20ce66d06c5523a8edfbeaa7428f1fb1457
Luciano <luchosr@gmail.com>
2018-04-20 15:09:30 -0300

    first commit

---
+++ HelloWorld/helloWorld.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.0;
+
+contract helloWorld {
+    string public word = "Hello Fucked World";
+}
+
+// si usamos public para word, obtenemos la variable
+// porque la hacemos pública.
+
+// Los botones azules son botones Getters,
+//que vas a ver en el compilador Remix.
+// Los Steer estan en Rojo, y te permiten
+// cambiar la info, editarla.
